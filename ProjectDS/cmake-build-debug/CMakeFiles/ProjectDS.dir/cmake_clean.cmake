66ff54dd448aa5ed5341465d012e40d7d428b19f
DarkTigerFAF <73562487+DarkTigerFAF@users.noreply.github.com>
2022-04-24 08:59:46 +0200

    Add files via upload

---
+++ ProjectDS/cmake-build-debug/CMakeFiles/ProjectDS.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectDS.dir/main.cpp.obj"
+  "ProjectDS.exe"
+  "ProjectDS.exe.manifest"
+  "ProjectDS.pdb"
+  "libProjectDS.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectDS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
